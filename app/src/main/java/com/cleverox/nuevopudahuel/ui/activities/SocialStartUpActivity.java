5a1e7002d3ce5e9b6a97f0bb9a7ad0534d357620
David Arilla <darilla@moddity.net>
2016-02-25 09:25:32 +0100

    SocialStartUp

--- app/src/main/java/com/cleverox/nuevopudahuel/ui/activities/SocialStartUpActivity.java
+++ app/src/main/java/com/cleverox/nuevopudahuel/ui/activities/SocialStartUpActivity.java
@@ -2,6 +2,9 @@ package com.cleverox.nuevopudahuel.ui.activities;
 
 
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.cleverox.nuevopudahuel.R;
 import com.cleverox.nuevopudahuel.base.BaseActivity;
@@ -12,6 +15,9 @@ import com.cleverox.nuevopudahuel.base.BaseActivity;
  */
 public class SocialStartUpActivity extends BaseActivity {
 
+    private ImageButton facebook, twitter, instagram;
+    private Button remember;
+
     @Override
     protected int getLayoutResource() {
         return R.layout.social_startup_activity;
@@ -19,6 +25,11 @@ public class SocialStartUpActivity extends BaseActivity {
 
     @Override
     protected void configView() {
+        facebook = $(R.id.social_facebook);
+        twitter = $(R.id.social_twitter);
+        instagram = $(R.id.social_instagram);
+
+        remember = $(R.id.social_remember);
 
     }
 }
