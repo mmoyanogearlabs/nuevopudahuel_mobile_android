4b419e9da303e4c9e2333e7c70330da9d94bc83b
David Arilla <darilla@moddity.net>
2016-02-25 13:59:59 +0100

    DashBoard, canvi strings

---
+++ app/src/main/java/com/cleverox/nuevopudahuel/ui/activities/DashBoardActivity.java
@@ -0,0 +1,19 @@
+package com.cleverox.nuevopudahuel.ui.activities;
+
+import com.cleverox.nuevopudahuel.R;
+import com.cleverox.nuevopudahuel.base.BaseActivity;
+
+/**
+ * Created by moddity on 25/2/16.
+ */
+public class DashBoardActivity extends BaseActivity {
+    @Override
+    protected int getLayoutResource() {
+        return R.layout.dash_board_activity;
+    }
+
+    @Override
+    protected void configView() {
+
+    }
+}
