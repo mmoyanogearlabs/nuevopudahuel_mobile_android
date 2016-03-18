d3ce17019d3af022262590f4774de6ae241db67e
Ivan Aguila <iaguila@moddity.net>
2016-03-18 12:42:54 +0100

    follow nuevo pudahuel rss

--- app/src/main/java/com/cleverox/nuevopudahuel/ui/activities/SocialStartUpActivity.java
+++ app/src/main/java/com/cleverox/nuevopudahuel/ui/activities/SocialStartUpActivity.java
@@ -1,8 +1,11 @@
 package com.cleverox.nuevopudahuel.ui.activities;
 
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.view.View;
+
 import com.cleverox.nuevopudahuel.R;
 import com.cleverox.nuevopudahuel.base.BaseActivity;
 import com.cleverox.nuevopudahuel.controllers.SyncController;
@@ -33,13 +36,50 @@ public class SocialStartUpActivity extends BaseActivity implements View.OnClickL
             case R.id.social_remember:
                 startActivity(new Intent(this, HomeActivity.class));
                 finish();
-                break;
+                return;
             case R.id.social_facebook:
+                startActivity(getOpenFacebookIntent());
                 break;
             case R.id.social_twitter:
+                openTwitterIntent();
                 break;
             case R.id.social_instagram:
+                openInstagramTwitter();
                 break;
+        }
+    }
+
+    private Intent getOpenFacebookIntent() {
+        try {
+            getPackageManager()
+                    .getPackageInfo("com.facebook.katana", 0); //Checks if FB is even installed.
+            return new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("fb://page/1199933116700243")); //Trys to make intent with FB's URI
+        } catch (Exception e) {
+            return new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://www.facebook.com/Aeropuerto-Santiago-AMB-Nuevo-Pudahuel-1199933116700243")); //catches and opens a url to the desired page
+        }
+    }
+
+    private void openTwitterIntent() {
+        try {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("twitter://user?screen_name=NuevoPudahuel")));
+        }catch (Exception e) {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com/#!/NuevoPudahuel")));
+        }
+    }
+
+    private void openInstagramTwitter() {
+        Uri uri = Uri.parse("http://instagram.com/_u/aeropuertoamb");
+        Intent likeIng = new Intent(Intent.ACTION_VIEW, uri);
+
+        likeIng.setPackage("com.instagram.android");
+
+        try {
+            startActivity(likeIng);
+        } catch (ActivityNotFoundException e) {
+            startActivity(new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("http://instagram.com/aeropuertoamb")));
         }
     }
 
