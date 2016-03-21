cecd076905c0811126bcda7d43da55834519bc5e
David Arilla <darilla@moddity.net>
2016-03-21 13:49:52 +0100

    canvi dimes, amagar editText

--- app/src/main/java/com/cleverox/nuevopudahuel/ui/fragments/FlightsFragment.java
+++ app/src/main/java/com/cleverox/nuevopudahuel/ui/fragments/FlightsFragment.java
@@ -1,5 +1,6 @@
 package com.cleverox.nuevopudahuel.ui.fragments;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -9,6 +10,7 @@ import android.text.TextWatcher;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
