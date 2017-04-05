cccf25c8b451b22ed151486f13404bf93c42be64
Jotta Emme <jmasensio@moddity.net>
2017-04-05 11:59:47 +0200

    ANP-130 Añadido botón mostrar y desactivados los eventos en los selectores. Activados eventos en botón mostrar Dialog se cierra también tras pulsar botón mostrar.

--- app/src/main/assets/maps/js/userInterface-es.js
+++ app/src/main/assets/maps/js/userInterface-es.js
@@ -201,13 +201,22 @@
             // pmr 
             tpl += '<div class="pmr-row"><input type="checkbox" name="pmr" title="Enable PRM mode" />' + kEnablePRMMode + '</div>';
         }
+        //btn btn mostrar, para hacer la búsqueda solo apretando el boton y no siempre que se elija el selector
+        tpl+='<div class="btn-cerrar-div"><input type="button" value="Mostrar" id="btn-mostrar-itinerario" name="btn-mostrar-itinerario" /></div>';
         
         tpl += '</div></div>';
         this.itinerarySelectorPanel = $(tpl);
 
-        this.itinerarySelectorPanel.on('change', 'select', $.proxy(this.itinerarySelectChange, this));
+        //Funciones que habrá que desencadenar solo en mostrar
         this.itinerarySelectorPanel.on('click', 'a.close-button', $.proxy(this.closeItineraryPanel, this));
-        this.itinerarySelectorPanel.on('change', ':input[name=pmr]', $.proxy( this.requestPmr, this));
+        //this.itinerarySelectorPanel.on('change', 'select', $.proxy(this.itinerarySelectChange, this));
+        //this.itinerarySelectorPanel.on('change', ':input[name=pmr]', $.proxy( this.requestPmr, this));
+
+        //Aplico estas acciones al botón
+        this.itinerarySelectorPanel.on('click', ':input[name=btn-mostrar-itinerario]',$.proxy(this.itinerarySelectChange, this));
+        this.itinerarySelectorPanel.on('click', ':input[name=btn-mostrar-itinerario]',$.proxy( this.requestPmr, this));
+        //Tambien cierra el panel
+        this.itinerarySelectorPanel.on('click', ':input[name=btn-mostrar-itinerario]',$.proxy(this.closeItineraryPanel, this));
     };
 
     ViewerBaseUi.prototype.requestPmr = function(ev) {
@@ -803,13 +812,24 @@ OfflineViewerUi.prototype.toString = function() {
             // pmr 
             tpl += '<div class="pmr-row"><input type="checkbox" name="pmr" title="Enable PRM mode" />Enable PRM mode</div>';
         }
+
+        //btn btn mostrar, para hacer la búsqueda solo apretando el boton y no siempre que se elija el selector
+        tpl+='<div class="btn-cerrar-div"><input type="button" value="Mostrar" id="btn-mostrar-itinerario" name="btn-mostrar-itinerario" /></div>';
         
         tpl += '</div></div>';
         this.itinerarySelectorPanel = $(tpl);
 
-        this.itinerarySelectorPanel.on('change', 'select', $.proxy(this.itinerarySelectChange, this));
+        //Funciones a cambiar el disparador
         this.itinerarySelectorPanel.on('click', 'a.close-button', $.proxy(this.closeItineraryPanel, this));
-        this.itinerarySelectorPanel.on('change', ':input[name=pmr]', $.proxy( this.requestPmr, this));
+
+        //this.itinerarySelectorPanel.on('change', 'select', $.proxy(this.itinerarySelectChange, this));
+        //this.itinerarySelectorPanel.on('change', ':input[name=pmr]', $.proxy( this.requestPmr, this));
+        
+        //Aplico estas acciones al botón
+        this.itinerarySelectorPanel.on('click', ':input[name=btn-mostrar-itinerario]',$.proxy(this.itinerarySelectChange, this));
+        this.itinerarySelectorPanel.on('click', ':input[name=btn-mostrar-itinerario]',$.proxy( this.requestPmr, this));
+        //Cierra el panel tambien
+        this.itinerarySelectorPanel.on('click', ':input[name=btn-mostrar-itinerario]',$.proxy(this.closeItineraryPanel, this));
     };
 
     ViewerBaseUi.prototype.requestPmr = function(ev) {
